ed1de7d28904e5256670fddb74fabe874feb6dd2
shayanypn <shayanypn@gmail.com>
2020-09-21 20:50:35 +0330

    Updates

---
+++ precache-manifest.6553d02ccda57cbb6604e94a19386fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62200b3009d712ca9be0971cbf9c88a5",
+    "url": "/simple-react-gallery/index.html"
+  },
+  {
+    "revision": "defeaab56e5252a41aa2",
+    "url": "/simple-react-gallery/static/css/main.4f3e1e27.chunk.css"
+  },
+  {
+    "revision": "269c4dc64795c6b2dc29",
+    "url": "/simple-react-gallery/static/js/2.dd56f0f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-gallery/static/js/2.dd56f0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defeaab56e5252a41aa2",
+    "url": "/simple-react-gallery/static/js/main.cc9acfc1.chunk.js"
+  },
+  {
+    "revision": "c2a604dd087d0ad6a633",
+    "url": "/simple-react-gallery/static/js/runtime-main.7859e53b.js"
+  }
+]);
